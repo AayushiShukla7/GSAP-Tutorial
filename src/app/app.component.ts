fd039983c1b556725baf0a1a345b0cd704435bad
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2025-06-24 18:13:13 -0400

    gsap.to() demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,27 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  
   title = 'GSAP-Tutorial';
+  @ViewChild('blueBox') blueBox!: ElementRef;
+    
+  ngAfterViewInit() {
+    //gsap.registerPlugin();
+
+    gsap.to(this.blueBox.nativeElement, { 
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      easy: 'bounce.out'
+    });
+
+    // const tl = gsap.timeline();
+
+    // tl.to(this.box.nativeElement, { duration: 1, x: 100, opacity: 1 })
+    //   .to(this.box.nativeElement, { duration: 1, rotation: 360 })
+    //   .to(this.box.nativeElement, { duration: 1, scale: 2 });
+  }
 }
